105f4f2f25e8da491f7974168db9cb91fed08643
priestly-sujan <priestlyraj@gmail.com>
2021-09-19 15:26:21 +0530

    my project

---
+++ emeregency/app/src/main/java/com/example/emeregency/HelpInfo.java
@@ -0,0 +1,15 @@
+package com.example.emeregency;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_info);
+    }
+}
